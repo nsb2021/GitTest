83a229a970ad788eba0b3d2fadcff76bdd7820ff
altuk <al.tukanov@gmail.com>
2021-10-01 20:56:45 +0400

    commit1_master

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,6 @@ public class Main {
         System.out.println("Enter size");
         int size = scn.nextByte();
         int[] arr = new int[size];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (int)(Math.random()*10);
-        }
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
+        System.out.println("hello master");
     }
 }
