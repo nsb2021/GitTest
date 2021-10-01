1a22b5db54adeaca090ca410590b97c51464aa97
Nasiba <6475650@gmail.com>
2021-10-01 12:34:13 -0400

    changed loop condition

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    int[] arr = {1,2,3};
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.lengthgit ; i++) {
             System.out.println(arr[i]);
         }
     }
